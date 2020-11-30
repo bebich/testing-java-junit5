9b0dd662df119ac75df5ad394a2a9624dfedbec4
fbebek <finassistapp@gmail.com>
2020-11-30 13:14:23 +0100

    DOC 1., Using mockito answers and Verify order of interactions

--- src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/controllers/OwnerControllerTest.java
@@ -1,16 +1,21 @@
 package guru.springframework.sfgpetclinic.controllers;
 
 import guru.springframework.sfgpetclinic.fauxspring.BindingResult;
+import guru.springframework.sfgpetclinic.fauxspring.Model;
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.mockito.*;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -23,10 +28,36 @@ class OwnerControllerTest {
     OwnerService ownerService;
 
     @Mock
+    Model model;
+
+    @Mock
     BindingResult bindingResult;
 
     @InjectMocks
     OwnerController ownerController;
+
+    @Captor
+    ArgumentCaptor<String> stringArgumentCaptor;
+
+    @BeforeEach
+    void setUp(){
+        given(ownerService.findAllByLastNameLike(stringArgumentCaptor.capture()))
+                .willAnswer(invocation -> {
+                    List<Owner> owners = new ArrayList<>();
+                    String name = invocation.getArgument(0);
+                    if(name.equals("%Buck%")){
+                        owners.add(new Owner(1L, "Joe", "Buck"));
+                        return owners;
+                    } else if(name.equals("%DontFindMe%")){
+                        return owners;
+                    } else if(name.equals("%FindMe%")){
+                        owners.add(new Owner(1L, "Joe", "Buck"));
+                        owners.add(new Owner(2L, "Joe", "Buck2"));
+                        return owners;
+                    }
+                    throw new RuntimeException("Invalid Argument");
+                });
+    }
 
     @Test
     void processCreationFormNoErrorsTest(){
@@ -57,5 +88,37 @@ class OwnerControllerTest {
 
     }
 
+    @Test
+    void processFindFormWildcardStringAnnotation() {
+        Owner owner = new Owner(5L, "Joe", "Buck");
 
+        String viewName = ownerController.processFindForm(owner, bindingResult, null);
+
+        assertThat("%Buck%").isEqualToIgnoringCase(stringArgumentCaptor.getValue());
+        assertThat("redirect:/owners/1").isEqualToIgnoringCase(viewName);
+    }
+
+    @Test
+    void processFindFormWildcardNotFound() {
+        Owner owner = new Owner(5L, "Joe", "DontFindMe");
+
+        String viewName = ownerController.processFindForm(owner, bindingResult, null);
+
+        assertThat("%DontFindMe%").isEqualToIgnoringCase(stringArgumentCaptor.getValue());
+        assertThat("owners/findOwners").isEqualToIgnoringCase(viewName);
+    }
+
+    @Test
+    void processFindFormWildcardFound() {
+        Owner owner = new Owner(5L, "Joe", "FindMe");
+        InOrder inOrder = inOrder(ownerService, model);
+
+        String viewName = ownerController.processFindForm(owner, bindingResult, model);
+
+        assertThat("%FindMe%").isEqualToIgnoringCase(stringArgumentCaptor.getValue());
+        assertThat("owners/ownersList").isEqualToIgnoringCase(viewName);
+
+        inOrder.verify(ownerService).findAllByLastNameLike(anyString());
+        inOrder.verify(model).addAttribute(anyString(), anyList());
+    }
 }
